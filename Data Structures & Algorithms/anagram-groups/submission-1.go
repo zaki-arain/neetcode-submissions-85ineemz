fd7c763fc014ba299215be7f1c6cf121dab14afe
Zaki <zaki.dev@pm.me>
2026-04-05 21:04:12 -0500

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.go
@@ -0,0 +1,22 @@
+func groupAnagrams(strs []string) [][]string {
+	var solution [][]string
+	table := make(map[[26]int][]string)
+
+	for _, str := range strs {
+		m := countLetters(str)
+		table[m] = append(table[m], str)
+	}
+	for _, v := range table {
+		solution = append(solution, v)
+	}
+
+	return solution
+}
+
+func countLetters(s string) [26]int {
+    smap := [26]int{}
+    for _, char := range s {
+        smap[char-'a']++ // 'a'-'a'=0, 'b'-'a'=1 ('a' has value 97)
+    }
+    return smap
+}
